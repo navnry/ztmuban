0687bcdc423c1a9caf7c884e79e8b2b282b3d814
navnry <1977234700@qq.com>
2019-09-24 15:53:56 +0800

    最后一次提交于2019-09-24

--- src/router.js
+++ src/router.js
@@ -1,26 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/Home.vue'
 import Content from './views/Content'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
+    {
+      path: '/',
+      name: 'content',
+      component: Content
+    },
     // {
     //   path:'/about',
     //   name:"about",
     //   component: resolve=>(require(["@/views/About"],resolve))
     // },
-    {
-      path:'/',
-      name:"content",
-      component: Content
-    }
   ]
 })
